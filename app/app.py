5b0012f04069516eb449bc2405d1ac97763af7db
mdmclean <mdmclean@users.noreply.github.com>
2022-10-11 17:07:55 +0000

    add flask web server

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from get_snippet_from_overcast import get_mp3_from_overcast
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add-clip', methods=['POST'])
+def add_clip():
+    content = request.get_json()
+    print(content['targetUrl'])
+    get_mp3_from_overcast(content['targetUrl'])
+    return '', 200
+
+app.run(host='0.0.0.0', port=81)
